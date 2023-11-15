c03f9a18a35ac4c97e603152d4d31b7b81ded2af
dimash228 <149990119+gaucho07@users.noreply.github.com>
2023-11-15 09:07:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-import pip install psycopg2
-hostname = 'localhost'
-database = 'python'
-username = 'postgres'
-pwd = '123'
-port_id = 5432
-conn = None
-cur = None 
-try:
-    conn = psycopg2. connect (
-                host = hostname,
-                dbname = database,
-                user = username,
-                password = pwd,
-                port = port_id)
-    cur = conn.cursor()
-    create_script = '''CREATE TABLE IF NOT EXISTS asd(
-                            id      int PRIMARY KEY,
-                            name    varchar(225) NOT NULL,
-                            salary  int,
-                            dept_id varchar(255))'''
-    cur.execute(create_script)
+#THIS IS NEW VERSION TO CONNECT TO POSTGRESQL
+import psycopg2
 
-    insert_script ='insert into asd (id, name, salary, dept_id) values (%s, %s, %s, %s)'
-    insert_value = (1, 'Sultan', 123, 'asd')
-    cur.execute(insert_script, insert_value)
+conn= psycopg2.connect(host = 'localhost',
+                       database = 'postgres',
+                       userpg = 'postgres',
+                       password = '123')
+cur = conn.cursor()
+cur.execute('SELECT superhero_name FROM superhero.superhero LIMIT 5;')
+usernames = [r[0] for r in cur.fetchall()]
+# Found= False
+# while not Found:
+#    username = input('Введите ваш логин:')
+#    if username in usernames:
+#       print('Вы есть в списке')
+#       Found=True
+#    else:
+#          print('Вас нет в списке')
 
-    conn.commit()
-except Exception as error:
-    print (error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+# def binary_search(username):
+#     return True
+#     return False
+# left, right = 0, len(rows) - 1
+# while left <= right:
+#     mid = (left + right) // 2
+#     mid_value = rows[mid] [0]
+# if mid_value ==
+conn.commit()
+cur.close()
+conn.close()
